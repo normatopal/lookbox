e2a9f5d1eacf57f1b5651f048636c06ac370ec15
Anton Topal <topalantt@gmail.com>
2017-01-05 23:11:14 +0200

    Styling for navbar

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,8 @@ ActiveRecord::Schema.define(version: 20160915182415) do
   create_table "pictures", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
-    t.integer  "user_id",     limit: 4
+    t.string   "user_id",     limit: 255
+    t.string   "bigint",      limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "image",       limit: 255
