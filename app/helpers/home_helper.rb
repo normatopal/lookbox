1dc65dff0616d248ff8f62bb3745b0430f7ad70a
norma <truenorma@gmail.com>
2018-12-11 18:46:42 +0200

    Into video from env, readme description

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def current_user_name
     user_signed_in? && current_user.name.present? ? current_user.name : "friend"
   end
+
+  def introduction_video_link
+    ENV['INTRO_VIDEO_ID'].present? ? "https://www.youtube.com/embed/#{ENV['INTRO_VIDEO_ID']}?rel=0&autoplay=0" : ''
+  end
 end
